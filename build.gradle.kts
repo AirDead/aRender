3caa32b42ca83f4bde4140c803d46bf6f9e1128e
AxieFeat <kirill@arial.su>
2024-08-26 00:58:02 +1000

    Deleted unused code

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,17 +13,6 @@ allprojects {
 	apply(plugin = "org.jetbrains.kotlin.jvm")
 }
 
-dependencies {
-	// Minecraft, Mappings, Fabric Loader
-	//minecraft("com.mojang:minecraft:${project.findProperty("minecraft_version")}")
-	//mappings("net.fabricmc:yarn:${project.findProperty("yarn_mappings")}:v2")
-	//modImplementation("net.fabricmc:fabric-loader:${project.findProperty("loader_version")}")
-
-	// Fabric API
-	//modImplementation("net.fabricmc.fabric-api:fabric-api:${project.findProperty("fabric_version")}")
-	//modImplementation("net.fabricmc.fabric-language-kotlin:${project.findProperty("fabric_kotlin_version")}")
-}
-
 tasks.processResources {
 	inputs.property("version", project.version)
 
